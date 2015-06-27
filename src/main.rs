4f3d6885397c1dd08ef34f853bfbf5c91cc844d9
Vaidas Pilkauskas <vaidasp@wix.com>
2015-06-27 21:06:57 +0300

    initial example

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#[macro_use] extern crate nickel;
+
+use std::env;
+use nickel::Nickel;
+
+fn main() {
+    let mut server = Nickel::new();
+
+    server.utilize(router! {
+        get "**" => |_req, _res| {
+             "Foobar!"
+        }
+    });
+
+    let addr = format!("{}:{}", env::var("HOST").unwrap(), env::var("PORT").unwrap());
+    server.listen(&*addr);
+}
+
